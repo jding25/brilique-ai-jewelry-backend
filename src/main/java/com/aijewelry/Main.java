d3f718bb4862bbde6a0a1dd8526e84526833e763
jding25 <jding25@berkeley.edu>
2025-07-21 11:11:48 -0700

    resolve time issue

--- src/main/java/com/aijewelry/Main.java
+++ src/main/java/com/aijewelry/Main.java
@@ -8,6 +8,9 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 import java.net.URI;
 
@@ -15,9 +18,13 @@ public class Main {
     public static final String BASE_URI = "http://0.0.0.0:8080/api/";
 
     public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(mapper);
         final ResourceConfig config = new ResourceConfig()
                 .register(DesignController.class)
-                .register(JacksonFeature.class)
+                .register(provider)
                 .register(CorsFilter.class);
 
         final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config, false);
